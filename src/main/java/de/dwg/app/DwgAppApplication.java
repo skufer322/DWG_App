3fbe1b35b52d2d26d026809f5f28854a224b1cca
Stefan Kufer <stefan.kufer@diva-e.com>
2021-01-27 08:55:55 +0100

    Initial commit

---
+++ src/main/java/de/dwg/app/DwgAppApplication.java
@@ -0,0 +1,13 @@
+package de.dwg.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwgAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DwgAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/dwg/app/DwgAppApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dwg.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwgAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
